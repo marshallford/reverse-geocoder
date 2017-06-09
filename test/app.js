e4d76e006afb3f3b851ed76162b71cb66dabf867
marshallford <inbox@marshallford.me>
2017-06-09 17:06:29 -0500

    a few reverse-geocode tests, async

--- test/app.js
+++ test/app.js
@@ -1,19 +1,38 @@
-/* global describe it */
+/* global describe it before */
+/* eslint-disable no-unused-expressions */
 import 'babel-polyfill'
 import chai from 'chai'
 import chaiHttp from 'chai-http'
 import app from '~/app'
+import client from '~/redis'
 
 chai.use(chaiHttp)
 const expect = chai.expect
 
-describe('status endpoint', () => {
-  it('responds', (done) => {
-    chai.request(app).get('/api/v1/status')
-      .end((err, res) => {
-        expect(err).to.be.undefined()
-        expect(res).to.have.status(200)
-      })
-    done()
+describe('status endpoint', async () => {
+  before(async () => {
+    await client.flushdbAsync()
+  })
+  it('responds correctly', async () => {
+    const res = await chai.request(app).get('/api/v1/status')
+    expect(res).to.have.status(200)
+    expect(res).to.be.json
+    expect(res.body.keys).to.equal(0)
+  })
+})
+
+describe('reverse-geocode endpoint', async () => {
+  before(async () => {
+    await client.flushdbAsync()
+  })
+  it('responds correctly', async () => {
+    const input = { lat: 50, lng: 50 }
+    const res = await chai.request(app).post('/api/v1/reverse-geocode').send(input)
+    expect(res).to.have.status(200)
+    expect(res).to.have.header('redis', 'MISS')
+    expect(res).to.be.json
+    expect(res.body).to.have.any.keys('input', 'output', 'extras', 'date_retrieved', 'provider', 'errors')
+    console.log(typeof res.body.input)
+    expect(res.body.input).to.deep.equal(input)
   })
 })
